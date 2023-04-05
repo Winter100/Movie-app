c5a91eb9f893aca47538b10f2a5b6252fc54a02e
winter100 <100taeng@gmail.com>
2023-04-05 19:40:33 +0900

    Add Comment Animation

--- src/components/Btn/Btn.jsx
+++ src/components/Btn/Btn.jsx
@@ -2,16 +2,14 @@ import style from "./Btn.module.css";
 
 function Btn({ setDropDownMenu }) {
   return (
-    <>
-      <div
-        className={style.menuTrigger}
-        onClick={() => setDropDownMenu((is) => !is)}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-    </>
+    <div
+      className={style.menuTrigger}
+      onClick={() => setDropDownMenu((is) => !is)}
+    >
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
   );
 }
 export default Btn;
