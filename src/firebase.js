910721b4db40334edeaf8d9ebbf2a282ef6e437d
winter100 <100taeng@gmail.com>
2023-04-05 17:56:41 +0900

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: `${process.env.REACT_APP_FIREBASE_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTHDOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_FIREBASE_DATABASEURL}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECTID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGEBUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APPID}`,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
